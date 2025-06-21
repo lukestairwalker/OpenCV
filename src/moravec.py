033f5a513abbcde9e22e3e8828b94df2fbf2a1ea
Jan Schultze <jan.l.schultze@gmail.com>
2025-06-21 16:46:13 +0200

    switched to VS code instead of Google Colab

---
+++ src/moravec.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def moravec_corner_detection(img_gray, window_size=3, threshold=10000):
+    corners = np.zeros_like(img_gray, dtype=np.float32)
+    offset = window_size // 2
+
+    for dy in [-1, 0, 1]:
+        for dx in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            shifted = np.roll(img_gray, shift=(dy, dx), axis=(0, 1))
+            ssd = (img_gray - shifted) ** 2
+            corners += cv2.boxFilter(ssd, -1, (window_size, window_size))
+
+    corners[corners < threshold] = 0
+    return corners#
